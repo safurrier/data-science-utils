6cb9736052fed1c1aad2beb71a8e0c68b9c40c19
safurrier <safurrier@gmail.com>
2018-09-13 10:34:20 -0700

    Added altair dark theme aesthetic support

--- code_gists/.ipynb_checkpoints/plot_aesthetics_for_dark_themes-checkpoint.py
+++ code_gists/.ipynb_checkpoints/plot_aesthetics_for_dark_themes-checkpoint.py
@@ -1,2 +1,6 @@
+## In Seaborn/Matplotlib
 import seaborn as sns
 sns.set(context='paper', style='darkgrid', rc={'figure.facecolor':'white'}, font_scale=1.2)
+
+## In altair
+alt.themes.enable('opaque')
