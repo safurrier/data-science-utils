def997053cbb7ab7cad21ae69c46516688abef40
safurrier <safurrier@gmail.com>
2019-02-14 10:26:11 -0700

    Updated data profiling gist to use Pathlib

--- gists/profile_data.py
+++ gists/profile_data.py
@@ -1,4 +1,5 @@
+import pathlib
 import pandas_profiling
-output_profile_path = r"reports\data-profiling\01-01-initial_data_profiling.html"
+output_profile_path = pathlib.Path("reports/data-profiling/01-01-initial_data_profiling.html")
 profile = pandas_profiling.ProfileReport(data)
-profile.to_file(outputfile=output_profile_path)
+profile.to_file(outputfile=output_profile_path.as_posix())
