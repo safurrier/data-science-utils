f297bdbb5d0336524048e6e4f85f5713b902ae8c
safurrier <safurrier@gmail.com>
2019-03-18 10:08:44 -0700

    Added convenience function param_string_to_kwargs_dict to ml

---
+++ ml/param_string_to_kwards_dict.py
@@ -0,0 +1,48 @@
+def param_string_to_kwargs_dict(multiline_param_string):
+    """ From a multiline parameter string of the form:
+    parameter
+    value
+    
+    Return a kwargs dictionary
+    
+    E.g. 
+    param_string = \"\"\"base_margin_initialize:
+    True
+    colsample_bylevel:
+    1.0
+    colsample_bytree:
+    0.5\"\"\"
+    
+    kwargs_param_dict = param_string_to_kwargs_dict(param_string)
+    kwargs_param_dict
+    {'base_margin_initialize': True,
+    'colsample_bylevel': 1.0,
+    'colsample_bytree': 0.5,
+    'interval': 10}
+    """
+    param_string
+    params = []
+    param_vals = []
+    for index, param in enumerate(multiline_param_string.split("\n")):
+        if (index == 0) | (index % 2 == 0):
+            params.append(param.replace(":", ""))
+        else:
+            # Get the python dtype of parameter value
+            # Cast to Numeric
+            try:
+                param = int(param)
+            except ValueError:
+                try:
+                    param = float(param)
+                except ValueError:
+                    pass
+            # Check for booleans
+            if param == 'True':
+                param = True
+            if param == 'False':
+                param = False            
+
+            param_vals.append(param)
+    # Create the dictionary
+    kwargs_params = dict(zip(params, param_vals))
+    return kwargs_params
