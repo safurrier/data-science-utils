4f558e4213a79dbb3d107d155f2516fb9e69cf0b
safurrier <safurrier@gmail.com>
2019-04-15 15:14:40 -0700

    Added function for computing mahalanobis distance under pandas.analysis.associative_relationship

--- pandas/analysis/.ipynb_checkpoints/associative_relationship-checkpoint.py
+++ pandas/analysis/.ipynb_checkpoints/associative_relationship-checkpoint.py
@@ -259,3 +259,25 @@ def target_mutual_information(df, target, target_type='categoric', columns=None,
     mmi_df = mmi_df[mmi_df['feature'] != mmi_df['paired_feature']]
     #display(mutual_information_df)
     return mmi_df     
+
+def mahalanobis(x=None, data=None, cov=None):
+    """Compute the Mahalanobis Distance between each row of x and the data  
+    x    : vector or matrix of data with, say, p columns.
+    data : ndarray of the distribution from which Mahalanobis distance of each observation of x is to be computed.
+    cov  : covariance matrix (p x p) of the distribution. If None, will be computed from data.
+    
+    Source: https://www.machinelearningplus.com/statistics/mahalanobis-distance/
+    
+    E.g.
+    df_x = df[['carat', 'depth', 'price']].head(500)
+    df_x['mahala'] = mahalanobis(x=df_x, data=df[['carat', 'depth', 'price']])
+    df_x.head()
+    """
+    x_minus_mu = x - np.mean(data)
+    if not cov:
+        cov = np.cov(data.values.T)
+    inv_covmat = sp.linalg.inv(cov)
+    left_term = np.dot(x_minus_mu, inv_covmat)
+    mahal = np.dot(left_term, x_minus_mu.T)
+    return mahal.diagonal()
+
