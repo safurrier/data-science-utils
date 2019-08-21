f2052f63af2ce494ebd29e8ea011c8a1decf9b90
safurrier <safurrier@gmail.com>
2019-08-21 11:51:24 -0700

    Made changes to run_notebook_report so that it can run a notebook without a config file, will execute a notebook inplace and can specify kernel for notebook to run on

--- gists/run_notebook_report.py
+++ gists/run_notebook_report.py
@@ -23,13 +23,14 @@ os.chdir(get_absolute_fpath())
 @click.command()
 @click.option('--notebook_import_path', default=None)
 @click.option('--config_path', default=None)
-@click.option('--parameterized_notebook_export_path', default=None)
+@click.option('--notebook_export_path', default=None)
 @click.option('--report_export_path', default=None)
 @click.option('--verbose', default=1)
 def run_notebook_report(notebook_import_path: str = None,
                         config_path: str = None,
-                        parameterized_notebook_export_path: str = None,
+                        notebook_export_path: str = None,
                         report_export_path: str = None,
+                        kernel_name: str = None,
                         verbose: int = None
                         ):
     """
@@ -42,23 +43,26 @@ def run_notebook_report(notebook_import_path: str = None,
     Parameters
     ----------
     notebook_import_path: str
-        A string path to the notebook to execute initially with papermill. 
-        Should be a notebook with references to a config file that is 
-        specified in a cell with a 'parameter tag'
-
+        A string path to the notebook to execute initially with papermill.
+        If using configurable variables, there should be a notebook with
+        references to a config file that is specified in a cell with a 'parameter' tag
     config_path: str
-        A string path to the configuration file that parameterizes the notebook at
+        An optional string path to the configuration file that parameterizes the notebook at
         `notebook_import_path`
-    parameterized_notebook_export_path: str
-        A string path designating the location to place the parameterized notebook at. 
-        Will be parameterized with papermill and exectued using nbconvert.      
+    notebook_export_path: str
+        A string path designating the location to place the executed notebook at.
+        If there is a config file, this notebook will be parameterized with papermill
+        and exectued using nbconvert.
     report_export_path: str
-        A string path designating the location to place executed html file at 
+        A string path designating the location to place executed html file at
+    kernel_name: str
+        The kernel name to execute the notebook with. Defaults to kernel found in 
+        notebook metadata. 
     verbose: int
 
     # TODO
     #######
-    # Options for 
+    # Options for
     # code cell execution present or not in report
     # Removing the parameterized notebook after executing it with nbconvert
 
@@ -66,50 +70,70 @@ def run_notebook_report(notebook_import_path: str = None,
     -------
     python run_notebook_report.py --notebook_import_path='notebooks/monthly_viz.ipnyb' \
     --config_path='config/monthly_viz_config.py' \
-    --parameterized_notebook_export_path='reports/monthly_viz/november.ipnyb'\
+    --notebook_export_path='reports/monthly_viz/november.ipnyb'\
     --report_export_path='reports/monthly_viz/november.html'
     """
 
     # Turn string paths into pathlib Paths
     notebook_import_path = pathlib.Path(notebook_import_path)
-    config_path = pathlib.Path(config_path)
-    parameterized_notebook_export_path = pathlib.Path(
-        parameterized_notebook_export_path)
+    if config_path:
+        config_path = pathlib.Path(config_path)
+
+    # If no export notebook is specified, run notebook and
+    # export back to original location
+    if not notebook_export_path:
+        notebook_export_path = notebook_import_path
+    else:
+        notebook_export_path = pathlib.Path(
+            notebook_export_path)
+
     report_export_path = pathlib.Path(report_export_path)
 
     if verbose:
         click.echo(
             f'Attempting to execute template notebook at {notebook_import_path.as_posix()}'
-            f' using papermill with paremeters set in config file at {config_path.as_posix()}\n'
+            f' using papermill'
         )
+        if config_path:
+            click.echo(
+                f'and parameters set in config file at {config_path.as_posix()}\n')
     # Execute notebook using papermill
-    pm.execute_notebook(
-        notebook_import_path.as_posix(),
-        parameterized_notebook_export_path.as_posix(),
-        # Run according to params
-        parameters=dict(
-            config_path=config_path.as_posix())
-    )
+    if config_path:
+        pm.execute_notebook(
+            notebook_import_path.as_posix(),
+            notebook_export_path.as_posix(),
+            # Run according to params
+            parameters=dict(
+                config_path=config_path.as_posix()),
+            kernel_name=kernel_name
+        )
+    else:
+        pm.execute_notebook(
+            notebook_import_path.as_posix(),
+            notebook_export_path.as_posix(),
+            kernel_name=kernel_name
+        )
+
     if verbose:
         click.echo(
-            f'Outputted executed notebook at {parameterized_notebook_export_path.as_posix()}\n'
+            f'Outputted executed notebook at {notebook_export_path.as_posix()}\n'
         )
     # %% [markdown]
     # Convert to html report
     resp = subprocess.run(
-        f"jupyter nbconvert {parameterized_notebook_export_path.as_posix()} --to=html --TemplateExporter.exclude_input=True")
+        f"jupyter nbconvert {notebook_export_path.as_posix()} --to=html --TemplateExporter.exclude_input=True")
 
     # If there was an error print
     if resp.check_returncode():
         click.echo(
-            f'Error converting {parameterized_notebook_export_path.as_posix()} to html report \n')
+            f'Error converting {notebook_export_path.as_posix()} to html report \n')
     # Otherwise move the outputted report and alert the use
     else:
         # # Remove old report if present
         # if os.path.isfile(report_export_path.as_posix()):
         #     os.remove(report_export_path.as_posix())
         # Move the outputted file to where the script specifies
-        os.rename(parameterized_notebook_export_path.as_posix().replace(
+        os.rename(notebook_export_path.as_posix().replace(
             "ipynb", "html"), report_export_path.as_posix())
 
         if verbose:
