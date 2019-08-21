ca2a851b750b0676ec617f19a192629dc76714d1
safurrier <safurrier@gmail.com>
2019-08-21 14:44:35 -0700

    Added a tests folder. First test functions are for checking keys and nested keys in a dictionary. Using to test if specific variables are in a loaded config file

---
+++ tests/dictionary/test_dictionary_keys.py
@@ -0,0 +1,36 @@
+def test_dictionary_keys(dictionary, required_keys, verbose=0):
+    missing_keys = []
+    for key in required_keys:
+        if key not in dictionary.keys():
+            missing_keys.append(key)
+    if missing_keys:
+        if verbose > 0:
+            print(
+                f'Missing keys {missing_keys} not found in dictionary with keys {dictionary.keys()}')
+        return False
+    else:
+        return True
+
+
+def test_nested_dictionary_keys(dictionary, nested_keys_dict, verbose=0):
+    missing_keys = []
+    missing_nested_keys = []
+    for key, nested_keys in nested_keys_dict.items():
+        # Check that all top level keys are in the dictionary
+        if key not in dictionary.keys():
+            missing_keys.append(key)
+        for nested_key in nested_keys:
+            if nested_key not in dictionary[key].keys():
+                missing_nested_keys.append(nested_key)
+    if missing_keys:
+        if verbose > 0:
+            print(
+                f'Missing keys {missing_keys} not found in dictionary with keys {dictionary.keys()}')
+        return False
+    if missing_nested_keys:
+        if verbose > 0:
+            print(
+                f'Nested keys: {missing_nested_keys} not found in dictionary.')
+        return False
+    else:
+        return True
