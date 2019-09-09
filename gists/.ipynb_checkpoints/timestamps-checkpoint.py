1337b908d8de7626fdddf0c60d2c3b33a41768e4
safurrier <safurrier@gmail.com>
2019-09-09 11:22:12 -0700

    Altered gists.now.py to gists.timestamps.py and added another commonly needed timestamp

---
+++ gists/.ipynb_checkpoints/timestamps-checkpoint.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+# Exact Time
+now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+# Todays date in lexicgraphical sort order
+today = datetime.now().strftime('%Y%m%d')
--- gists/now.py
+++
@@ -1,2 +0,0 @@
-from datetime import datetime
-now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
